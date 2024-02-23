3763ddc03191b666ee191a80d19d9348546b8275
Skyrus1305 <dagorin7@gmail.com>
2024-02-23 12:38:58 -0500

    proyecto creado

---
+++ src/model/Instructor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ * @author
+ ** Miguel Angel Naranjo Joya
+ ** Laura Andrea Riobueno Rincon
+ ** Cristian Camilo Tuso Mozo
+ * 
+ * @version 1.0 23/02/2024
+ * 
+ */
+public class Instructor {
+    
+}
