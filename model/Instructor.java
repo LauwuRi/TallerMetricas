25e10e85a0ccb5451745c49b763f0883386e0397
CristianTuso <161164397+CristianTuso@users.noreply.github.com>
2024-02-25 21:30:06 -0500

    Estoy cansado jefe

---
+++ model/Instructor.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * @author * Miguel Angel Naranjo Joya * Laura Andrea Riobueno Rincon * Cristian
+ * Camilo Tuso Mozo
+ *
+ * @version 1.0 23/02/2024
+ *
+ */
+public class Instructor extends Empleado {
+
+    public Instructor(String cedula, String nombre, String telefono, String fechaNacimiento, String direccion, String correo) {
+        super(cedula, nombre, telefono, fechaNacimiento, direccion, correo);
+    }
+
+    public Instructor() {
+    }
+
+    public Instructor(String cedula) {
+
+    }
+}
